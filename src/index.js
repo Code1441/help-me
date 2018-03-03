d7ac99bf9365a96d9ff4bfc29e0493c92915a128
TatsianaGelahova <tgelahova99@gmail.com>
2018-03-03 18:31:58 +0200

    :

--- src/index.js
+++ src/index.js
@@ -1,105 +1,27 @@
-module.exports =   function count(s, pairs) {
-    //(-_-;) (-_-'') (-_-;) (-_-'')
-    if(pairs[0][1]=== 1000000000) return;
-    if(s=='111100101000' ||  s=='0000000010' || s=='0000000000000000000000000000000000000000') return;
-    if(pairs[0][0]=== 11 ) return;
-    if( pairs[0][1]===128864793) return;
-    if( pairs[0][1]===8939193) return;
+module.exports = function count(s, pairs) {
 
-    //ф-я возвращает объект, где ключ - это простой делитель, значение - его степень
-    //для этого способа решения не нужна!!
-    // function toSimpleObject(num){
-    //     var object = {};
-    //     var count = 2;
-    //     while (num > 1) {
-    //         if (num % count === 0) {
-    //             if(object[count] === undefined) {
-    //                 object[count] = 1;
-    //             }
-    //             else object[count] += 1;
-    //             num /= count;
-    //         }
-    //         else count++;
-    //     }
-    //     return object;
-    // }
+     var objectN = {};
+     var result = 1;
+     var bool = true;
 
-    //
-    function isDivider(number, N){
+     for (var i = 0; i < pairs.length; i++) {
+         objectN[pairs[i][0]] = pairs[i][1];
+     }
 
-        var temp = number;
-        var obj = {};
-        if(temp===0) return false;
-        if(temp ===1) return true;
-        for(var prop in N){
-            while(temp%prop === 0) {
-                if(obj[prop] === undefined) {
-                    obj[prop] = 1;
-                }
-                else obj [prop] += 1;
-                temp /= prop;
-            }
-        }
+     i = 0;
 
-        if(objectEquality(obj, N)) return 14;
+     while (s[i]) {
+         if (s[i] !== '1') bool = false;
+         i++;
+     }
 
-        for(var prop in N) {
+     var length = s.length;
+     if (bool === true) {
+         for (var prop in objectN) {
+             var answer = (Math.pow(prop, objectN[prop]) - Math.pow(prop, objectN[prop] - 1));
+             result *= answer - length + 1;
+         }
+     }
 
-            if (number % prop === 0) return false;
-
-        }
-        return true;
-
-    }
-
-
-    function objectEquality(o1, o2){
-        if(Object.keys(o1).length !== Object.keys(o2).length) return false;
-        for(var prop in o1){
-            if(o1[prop]!==o2[prop]) return false;
-        }
-        return true;
-    }
-
-    var objectN = {};
-    var result1 = [], result2 = [], mainResult = [];
-    var k;
-
-    for(var i = 0; i < pairs.length; i++){
-        objectN[pairs[i][0]] = pairs[i][1];
-    }
-
-    for(var j = 0; j < s.length; j++){
-        if(s[j]==='1'){
-            k = 0;
-            while(true){
-                var answer = isDivider(k+j, objectN);
-                if(answer===14) break;
-                if(answer === true && result1.indexOf(k)===-1 ) result1.push(k);
-                k++;
-            }
-        }
-        else if(s[j]==='0'){
-            k = 0;
-            while(true){
-                var answer = isDivider(k+j, objectN);
-                if(answer===14) break;
-                if(answer === false && result2.indexOf(k)===-1) result2.push(k);
-                k++;
-            }
-        }
-    }
-
-    if(result1.length === 0) return result2.length% 1000000007;
-    else if (result2.length === 0) return result1.length % 1000000007;
-
-    for (var i = 0; i < result1.length; i++){
-        if(result2.indexOf(result1[i])!==-1 && mainResult.indexOf(result1[i])===-1) mainResult.push(result1[i]);
-    }
-    for (var i = 0; i < result2.length; i++){
-        if(result1.indexOf(result2[i])!==-1 && mainResult.indexOf(result2[i])===-1) mainResult.push(result2[i]);
-    }
-
-    return mainResult.length % 1000000007;
-}
-
+     return result % 1000000007;
+ }
