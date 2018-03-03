dde92626c5d5380f399b75c9962e7f50220b0362
TatsianaGelahova <tgelahova99@gmail.com>
2018-03-03 20:21:29 +0200

    feature: Euler formula solution

--- src/index.js
+++ src/index.js
@@ -1,27 +1,43 @@
 module.exports = function count(s, pairs) {
+    var N = 1;
+    var objectN = {};
+    var result = 1;
+    var bool = true;
+    
+    function numberPrimers(objectN) {
+        for (var prop in objectN) {
+            var answer = (Math.pow(prop, objectN[prop]) - Math.pow(prop, objectN[prop] - 1));
+            result *= answer - length + 1;
+        }
+        return result;
+    }
+    
+    
+    for (var i = 0; i < pairs.length; i++) {
+        objectN[pairs[i][0]] = pairs[i][1];
+    }
 
-     var objectN = {};
-     var result = 1;
-     var bool = true;
+    i = 0;
 
-     for (var i = 0; i < pairs.length; i++) {
-         objectN[pairs[i][0]] = pairs[i][1];
-     }
+    while (s[i]) {
+        if (s[i] !== '1') bool = false;
+        i++;
+    }
 
-     i = 0;
+    var length = s.length;
+    if (bool === true) {
+        result = numberPrimers(objectN);
+    }
 
-     while (s[i]) {
-         if (s[i] !== '1') bool = false;
-         i++;
-     }
+    // решение для s = ['0'] надо сделать с помощью формулы включений и исключений;
+    // пока так
+    if(bool == false && length === 1){
+        for(var i = 0; i < pairs.length; i++){
+            N *= (pairs[i][0] ** pairs[i][1]);
+        }
+        result = N - numberPrimers(objectN);
+        
+    }
 
-     var length = s.length;
-     if (bool === true) {
-         for (var prop in objectN) {
-             var answer = (Math.pow(prop, objectN[prop]) - Math.pow(prop, objectN[prop] - 1));
-             result *= answer - length + 1;
-         }
-     }
-
-     return result % 1000000007;
- }
+    return result % 1000000007;
+}
