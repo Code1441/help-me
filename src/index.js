d8c42f7ae039874b87486114febe2f0b7d867c0d
TatsianaGel <tgelahova99@mail.ru>
2018-03-05 19:01:51 +0300

    feture: combined 0 and 1

--- src/index.js
+++ src/index.js
@@ -1,43 +1,40 @@
-module.exports = function count(s, pairs) {
+module.exports =  function count(s, pairs) {
     var N = 1;
     var objectN = {};
-    var result = 1;
+    var result = [];
+    var resultArray;
     var bool = true;
-    
-    function numberPrimers(objectN) {
+
+    function numberPrimers(objectN, index) {
+        var result = 1;
         for (var prop in objectN) {
             var answer = (Math.pow(prop, objectN[prop]) - Math.pow(prop, objectN[prop] - 1));
-            result *= answer - length + 1;
+            result *= answer - index;
         }
         return result;
     }
-    
-    
+
+
     for (var i = 0; i < pairs.length; i++) {
         objectN[pairs[i][0]] = pairs[i][1];
     }
 
     i = 0;
 
-    while (s[i]) {
-        if (s[i] !== '1') bool = false;
-        i++;
-    }
-
     var length = s.length;
-    if (bool === true) {
-        result = numberPrimers(objectN);
+
+    if (s.indexOf('1')!==-1 ) {
+        result.push(numberPrimers(objectN, s.lastIndexOf('1')));
     }
 
     // решение для s = ['0'] надо сделать с помощью формулы включений и исключений;
     // пока так
-    if(bool == false && length === 1){
+    if(s.indexOf('0')!==-1 ){
         for(var i = 0; i < pairs.length; i++){
             N *= (pairs[i][0] ** pairs[i][1]);
         }
-        result = N - numberPrimers(objectN);
-        
+        result.push(N - numberPrimers(objectN, s.lastIndexOf('0')));
     }
 
-    return result % 1000000007;
+    return Math.min.apply(null,result) % 1000000007;
 }
